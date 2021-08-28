dd149b2d0d5924db20bbd05f83362f6a3331e66e
M-Hidayatullah <hidayatullahblk@gmail.com>
2021-08-28 23:14:34 +0800

    integrasi template stisla

---
+++ resources/views/home.blade.php
@@ -0,0 +1,21 @@
+@extends('layouts.main')
+@section('content')
+<section class="section">
+    <div class="section-header">
+        <h1>Dashboard</h1>
+    </div>
+    <div class="row">
+        <div class="col-lg-3 col-md-6 col-sm-6 col-12">
+            <div class="card card-statistic-1">
+                <div class="card-icon bg-primary">
+                    <i class="fa fa-book-open text-white fa-2x"></i>
+                </div>
+                <div class="card-wrap">
+                    <div class="card-header">
+                        <h4>Contoh</h4>
+                    </div>
+                    <div class="card-body">
+                        10
+                    </div>
+            </section>
+@endsection
